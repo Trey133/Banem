d1c91cd9a9ce8bb8055c3c0da4ed9d45ed59399d
root <root@localhost.localdomain>
2018-07-04 00:26:14 -0500

    update

--- init_auth.js
+++
@@ -1,8 +0,0 @@
-var admin = require('firebase-admin');
-
-var serviceAccount = require('banem-a2ae6-firebase-adminsdk-gjgig-0f3b5cb34b.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
-});
